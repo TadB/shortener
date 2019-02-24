318196ef74c17193318de0749a7859f07319509c
acny <tadeusz.brzytwa@gmail.com>
2019-02-24 23:51:02 +0100

    Change short field lenght.

--- shortener/models.py
+++ shortener/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 
 class Link(models.Model):
-    short = models.CharField(max_length=400)
+    short = models.CharField(max_length=64)
     full_url = models.URLField(max_length=800, unique=True)
